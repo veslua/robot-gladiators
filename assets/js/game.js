210cf4434bfde85e44397dda5fa7251e8a0cfc3d
L Gonsalves <gonsalves.luan@gmail.com>
2021-04-25 21:57:55 -0400

    added variables to js file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,41 @@
-window.alert("This is an alert! Javascript is running!");
+// this creates a function named "fight"
+
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("Our robot's name " + playerName);
+function fight() {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fight();
